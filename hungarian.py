452d17f4e92c472fc4ad815dd0f7c77e54f59b26
Felix <felix@lagnohed.se>
2021-12-12 13:54:30 +0100

    fixed bug + cleanup on aisle 4

--- hungarian.py
+++ hungarian.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import random
 
 
 def reduce_matrix(cost_matrix):
@@ -33,8 +34,7 @@ def find_min_coord(bool_matrix):
     In: bool_matrix (np.array)
     Out: [row_i (int), col_i (int)]
     """
-    r_dim = np.shape(bool_matrix)[0]
-    c_dim = np.shape(bool_matrix)[1]
+    r_dim, c_dim = np.shape(bool_matrix)
     zero_count_r = np.inf
     zero_count_c = np.inf
     row_i = np.inf
@@ -46,9 +46,7 @@ def find_min_coord(bool_matrix):
         if 0 < row_count < zero_count_r:  # hittade en ny bästa rad
             zero_count_r = row_count
             row_i = r
-
     col_i = np.where(bool_matrix[row_i])[0][0]
-
     # nu kollar vi om vi hittar en bättre kolumn än den rad vi har
     for c in range(c_dim):
         col_count = 0
@@ -71,8 +69,7 @@ def get_bool_matrix(reduced_matrix):
     Out: bool_m (np.array)
     """
     bool_m = []
-    r_dim = np.shape(reduced_matrix)[0]
-    c_dim = np.shape(reduced_matrix)[1]
+    r_dim, c_dim = np.shape(bool_matrix)
     for r in range(r_dim):
         bool_m_row = []
         for c in range(c_dim):
@@ -91,9 +88,7 @@ def get_marked_indexes(reduced_matrix):
     In: reduced_matrix (np.array)
     Out: [marked (list), binary_m (np.array)]
     """
-
-    r_dim = np.shape(reduced_matrix)[0]
-    c_dim = np.shape(reduced_matrix)[1]
+    r_dim, c_dim = np.shape(bool_matrix)
     bool_m = get_bool_matrix(reduced_matrix)
     binary_m = np.zeros((r_dim, c_dim), dtype=int)
     marked = []
@@ -101,43 +96,37 @@ def get_marked_indexes(reduced_matrix):
         min_row_i, col_i = find_min_coord(bool_m)
         num_of_rows = 0
         num_of_cols = 0
-        for i in range(r_dim):
-            if bool_m[min_row_i, i]:
+        for k in range(r_dim):
+            if bool_m[min_row_i, k]:
                 num_of_rows += 1
-            if bool_m[i, col_i]:
+            if bool_m[k, col_i]:
                 num_of_cols += 1
-
         if num_of_rows > num_of_cols:  # rita horisontellt streck i binary_m
             binary_m[min_row_i, :] = np.ones(c_dim)
         else:
             binary_m[:, col_i] = np.ones(r_dim)
-
         marked.append([min_row_i, col_i])
-        for i in range(r_dim):
-            bool_m[min_row_i, i] = False
-            bool_m[i, col_i] = False
-
+        for j in range(r_dim):
+            bool_m[min_row_i, j] = False
+            bool_m[j, col_i] = False
     check_again = True
-    uncovered_row = 0
-    uncovered_col = 0
+    uncovered = []
+    for r in range(len(reduced_matrix)):
+        for c in range(len(reduced_matrix)):
+            if not binary_m[r, c]:
+                uncovered += [reduced_matrix[r, c]]
+    if uncovered:
+        min_temp = min(x for x in uncovered if x > 0)
+    else:
+        min_temp = 0
     while check_again:
         check_again = False
         for r in range(r_dim):
             for c in range(c_dim):
                 if not reduced_matrix[r, c] and not binary_m[r, c]:  # not covered 0 found
                     check_again = True
-                    for col in range(len(reduced_matrix[r, :])):
-                        if not reduced_matrix[r, col] and not binary_m[r, col]:
-                            uncovered_row += 1
-                    for row in range(len(reduced_matrix[:, c])):
-                        if not reduced_matrix[row, c] and not binary_m[row, c]:
-                            uncovered_col += 1
-                    if uncovered_row > uncovered_col:
-                        binary_m[r, :] = np.ones(r_dim)
-                    else:
-                        binary_m[:, c] = np.ones(c_dim)
+                    reduced_matrix[r, c] += min_temp
                     break
-
     return marked, binary_m
 
 
@@ -146,7 +135,6 @@ def is_at_intersection(binary_matrix, coord):
 
 
 def shift_zeros(reduced_matrix, binary_matrix):
-    has_changed = False
     not_covered = []
     r_dim = np.shape(binary_matrix)[0]
     c_dim = np.shape(binary_matrix)[1]
@@ -154,18 +142,13 @@ def shift_zeros(reduced_matrix, binary_matrix):
         for c in range(c_dim):
             if not binary_matrix[r, c]:  # inte täckt
                 not_covered.append(reduced_matrix[r, c])  # lägg till det faktiska värdet
-
     min_val = min(not_covered)
     for r in range(r_dim):
         for c in range(c_dim):
             if not binary_matrix[r, c]:
-                has_changed = True
                 reduced_matrix[r, c] -= min_val
             if binary_matrix[r, c] and is_at_intersection(binary_matrix, (r, c)):
                 reduced_matrix[r, c] += min_val
-    if has_changed:
-        pass
-        # print("HAR ÄNDRATS")
     return reduced_matrix
 
 
@@ -180,71 +163,30 @@ def unique_rows(marked):
     return min(len(rows), len(cols))  # will differ if we have multiple positions on the same line
 
 
-def hungarian():
-    """
-    Hämtar dimension och matris från input-fil.
-    [python3 hungarian.py < INPUTFILE]
-    """
+def hungarian(matr):
     debug_list = []
-
-    r_dim = int(sys.stdin.readline())
-
-    inp = []
-    for i in range(r_dim):
-        i_r = []
-        snigel = str(sys.stdin.readline())
-        for j in range(len(snigel)):
-            if snigel[j] != '\n' and snigel[j] != ' ':
-
-                x = int(snigel[j])
-                i_r.append(x)
-        inp.append(i_r)
-
-    test_cost = np.array(inp)
-
-    # testmatriser
-    # test_cost_m = np.matrix([[1, 2, 3, 4], [5, 2, 9, 6], [1, 2, 2, 4], [8, 3, 7, 2]])
-    # kattis_easy = np.array([[2, 1, 2, 2], [1, 2, 2, 2], [2, 2, 1, 2], [2, 2, 2, 1]])
-    # kattis_hard = np.array([[5, 1, 1], [5, 4, 9], [6, 2, 5]])
-    # kattis_hard2 = np.array([[5, 5, 6], [1, 4, 2], [1, 9, 5]])
-    # ha_test = np.array([[82, 83, 69, 92], [77, 37, 49, 92], [11, 69, 5, 86], [8, 9, 98, 23]])
-    # test_rand = np.array(np.random.randint(0, 10, (24, 26)))  # (tasks, units)
-    # fail_matrix = np.array([[0, 8, 7, 7, 5, 7], [0, 6, 9, 8, 4, 5], [9, 6, 3, 1, 3, 3], [0, 1, 9, 9, 7, 1]])
-    # cost_matrix = kattis_easy
-    # cost_matrix = kattis_hard2
-    # cost_matrix = ha_test
-    cost_matrix = test_cost
-    reduced_matrix = reduce_matrix(cost_matrix)
+    reduced_matrix = reduce_matrix(matr)
     total_cost = 0
     r_dim = len(reduced_matrix)
-    orig_r = len(cost_matrix)
-    orig_c = len(cost_matrix[0])
+    orig_r = len(matr)
+    orig_c = len(matr[0])
     lines = 0
     positions = []
     while lines < r_dim:
-
         positions, bin_m = get_marked_indexes(reduced_matrix)
         lines = unique_rows(positions)
-        print(lines, r_dim)
-        print(positions)
         if lines < r_dim:
             # steg 4
             reduced_matrix = shift_zeros(reduced_matrix, bin_m)
-    print(positions)
     for pos in sorted(positions, key=lambda p: p[0]):
         if pos[0] < min(orig_r, orig_c):
-
             debug_list.append(pos[1])
-            total_cost += cost_matrix[pos[0], pos[1]]
-
-    i = 0
-    outp = ""
-    while i < len(debug_list)-1:
-        outp += str(debug_list[i]) + " "
-        i += 1
-    outp += str(debug_list[-1]) + "\n"
+            total_cost += matr[pos[0]][pos[1]]
     print(total_cost)
-    print(outp)
+    print(debug_list)
 
 
-hungarian()
+mat = [[[random.randint(0, 10) for x in range(50)] for y in range(50)]for _ in range(100)]
+for i, m in enumerate(mat):
+    print(i)
+    hungarian(m)
